5501fcb692000e4e4565417500913f42496271bb
siddharthaerpa-dot <siddharthaerpa@gmail.com>
2026-04-13 18:38:46 +0530

    Update player.jsx

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,22 +1,14 @@
-import { useContext } from "react";
-import { PlayerContext } from "../context/PlayerContext";
-
-function Player() {
-  const { currentSong } = useContext(PlayerContext);
-
-  if (!currentSong) return null;
+function Player({ song }) {
+  if (!song) return null;
 
   return (
-    <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-[90%] md:w-[60%] glass p-4 flex justify-between items-center shadow-lg">
-      
-      <div>
-        <p className="font-bold">{currentSong.name}</p>
-        <p className="text-sm text-gray-300">{currentSong.mood}</p>
-      </div>
-
-      <div className="text-green-400 animate-pulse">
-        ▶ Playing
-      </div>
+    <div className="fixed bottom-0 left-0 w-full bg-black p-3 flex justify-center">
+      <iframe
+        src={song.url}
+        width="300"
+        height="80"
+        allow="autoplay; encrypted-media"
+      ></iframe>
     </div>
   );
 }
